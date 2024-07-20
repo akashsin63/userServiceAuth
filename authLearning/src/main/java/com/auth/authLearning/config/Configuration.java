ff61a157798cad6482cb1e8f97d28c16b87aa437
Akash <akashsingh54812@gmail.com>
2024-07-20 17:42:03 +0530

    basics of spring security , Tired to bypass authorization

---
+++ authLearning/src/main/java/com/auth/authLearning/config/Configuration.java
@@ -0,0 +1,29 @@
+package com.auth.authLearning.config;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.context.annotation.RequestScope;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+	 @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+               .authorizeHttpRequests((requests) -> {
+                           try {
+                               requests
+                                       .anyRequest().permitAll()
+                                       .and().cors().disable()
+                                       .csrf().disable();
+                           } catch (Exception e) {
+                               throw new RuntimeException(e);
+                           }
+                       }
+               );
+
+       return http.build();
+   }
+}
